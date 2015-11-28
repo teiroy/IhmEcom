878ea98ed7d84928196b0d13ea1bbcddff516731
tararaina <teiroy@gmail.com>
2015-11-28 16:35:04 +0100

    Initialisation Projet IHM en relation avec le projet E-com

---
+++ app/controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module("appControllers", [])
+
+.controller('Ctrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    };
+})
+
+.controller('DrawingSpace', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    };
+})
